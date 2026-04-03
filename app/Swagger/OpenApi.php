de40836dae95ee43e0c143b35ec0885c37b7c401
priyanka coderth <priyanka@coderth.com>
2026-04-03 12:15:52 +0530

    authorize

--- app/Swagger/OpenApi.php
+++ app/Swagger/OpenApi.php
@@ -9,18 +9,22 @@ use OpenApi\Annotations as OA;
  *     title="Referral API",
  *     version="1.0.0"
  * )
+ * 
  * @OA\SecurityScheme(
- *      securityScheme="Bearer",
- *      type="http",
- *      scheme="bearer"
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
  * )
+ * 
  * @OA\Server(
- *      url=L5_SWAGGER_CONST_HOST,
- *      description="API Server"
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="API Server"
  * )
+ * 
  * @OA\Tag(
- *      name="Auth",
- *      description="Authentification",
+ *     name="Auth",
+ *     description="Authentication"
  * )
  */
 class OpenApi {}
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-Route::post('/refresh-token', [AuthController::class, 'refreshToken'])
-    ->middleware('auth:sanctum');
+});
