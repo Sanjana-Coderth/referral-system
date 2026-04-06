ceec4573296422b7e269e0e95e2f0cc2c390c418
sanjana coderth <sanjana@coderth.com>
2026-04-06 10:23:33 +0530

    update token

--- database/migrations/2026_04_06_041834_create_personal_access_tokens_table.php
+++ database/migrations/2026_04_06_041834_create_personal_access_tokens_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuidMorphs('tokenable');
+
+            $table->string('tokenable_type');
+            $table->uuid('tokenable_id');
+
             $table->text('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
